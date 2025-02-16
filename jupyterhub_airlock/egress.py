1c32f28de05d68cc05d9995c2bf0108124d21dec
Simon Li <orpheus+devel@gmail.com>
2025-02-16 19:32:48 +0000

    WIP

---
+++ jupyterhub_airlock/egress.py
@@ -0,0 +1,156 @@
+from enum import Enum
+import hashlib
+from pathlib import Path
+from typing import Any, Dict, List, TypeAlias
+import json
+import re
+
+JsonT: TypeAlias = Any
+
+EGRESS_FILE_DIR = "files"
+EGRESS_ID_PATTERN = r"^[A-Za-z0-9][A-Za-z0-9-]+$"
+
+
+class EgressStatus(Enum):
+    NEW = "new"
+    CANCELLED = "cancelled"
+    PENDING = "pending"
+    ACCEPTED = "accepted"
+    REJECTED = "rejected"
+
+    def __str__(self) -> str:
+        return str(self.value)
+
+
+EgressStatus_transitions = {
+    EgressStatus.NEW: {EgressStatus.PENDING, EgressStatus.CANCELLED},
+    EgressStatus.CANCELLED: {},
+    EgressStatus.PENDING: {EgressStatus.ACCEPTED, EgressStatus.REJECTED},
+    EgressStatus.ACCEPTED: {},
+    EgressStatus.REJECTED: {},
+}
+
+
+def is_valid_egress_id(id: str, raise_invalid: bool = True) -> bool:
+    """
+    Checks whether an egress id string is allowed
+    """
+    if not re.match(EGRESS_ID_PATTERN, id):
+        if not raise_invalid:
+            return False
+        raise ValueError(f"Invalid egress id {id}")
+    return True
+
+
+class Egress:
+    _egress_status_values = set(e.value for e in EgressStatus)
+
+    def __init__(self, id: str, path: Path, create: bool = False):
+        is_valid_egress_id(id)
+        self.id = id
+        self.path = path
+        if create:
+            if self.path.exists():
+                raise ValueError(f"Egress {id} already exists")
+            self.path.mkdir()
+            (self.path / "files").mkdir()
+            self.update_metadata(
+                {"id": id, "status": EgressStatus.NEW.value, "files": []},
+                create=True,
+            )
+
+    def metadata(self) -> JsonT:
+        """
+        Returns egress metadata
+        """
+        m = self.path / "metadata.json"
+        if not m.exists() or not m.is_file():
+            raise ValueError(f"Metadata for egress {self.id} not found")
+        return json.loads(m.read_text())
+
+    def update_metadata(self, d: Dict[str, object], create: bool = False) -> None:
+        """
+        Update some metadata key-value pairs
+        """
+        if create:
+            m = {}
+        else:
+            m = self.metadata()
+        for k, v in d.items():
+            m[k] = v
+        json_m = json.dumps(m, indent=2)
+        metadata_file = self.path / "metadata.json"
+        metadata_file.write_text(json_m)
+
+    def status(self) -> EgressStatus:
+        """
+        Get the egress status
+        """
+        m = self.metadata()
+        status = m["status"]
+        return EgressStatus(status)
+
+    def set_status(self, status: EgressStatus) -> None:
+        """
+        Change the egress status
+        """
+        current = self.status()
+        if status not in EgressStatus_transitions[current]:
+            raise ValueError(
+                f"Current status {current}, new status {status} is invalid"
+            )
+        self.update_metadata({"status": status.value})
+
+    def add_files(self) -> None:
+        """
+        Updates egress metadata.json with the recursive list of files in this directory
+        """
+        current = self.status()
+        if current != EgressStatus.NEW:
+            raise ValueError(
+                f"Can only add files to status {EgressStatus.NEW}, current status {current}"
+            )
+        files = []
+        d = self.path / EGRESS_FILE_DIR
+        for f in d.rglob("*"):
+            if f.is_dir():
+                continue
+            if not f.is_file():
+                raise ValueError(f"Not a standard file: {f}")
+            with open(str(f), "rb") as fh:
+                h = hashlib.file_digest(fh, hashlib.sha256)
+                files.append(
+                    {"path": str(f.relative_to(d)), "sha256sum": h.hexdigest()}
+                )
+        self.update_metadata({"files": files})
+        self.set_status(EgressStatus.PENDING)
+
+
+class EgressStore:
+    def __init__(self, filestore: Path):
+        self.filestore: Path = filestore
+
+    def list(self) -> List[str]:
+        """
+        List all egresses
+        """
+        dirs = [f.relative_to(self.filestore) for f in self.filestore.glob("*/")]
+        return sorted(str(d) for d in dirs if is_valid_egress_id(str(d)))
+
+    def get_egress(self, egress_id: str) -> Egress:
+        """
+        Get an egress
+        """
+        is_valid_egress_id(egress_id)
+        e = self.filestore / egress_id
+        if not e.exists() or not e.is_dir():
+            raise ValueError(f"Egress {egress_id} not found")
+        return Egress(egress_id, e)
+
+    def new_egress(self, egress_id: str) -> Egress:
+        """
+        Create a new empty egress
+        """
+        e = self.filestore / egress_id
+        egress = Egress(egress_id, e, create=True)
+        return egress
---
+++ tests/test_egressstore.py
@@ -0,0 +1,116 @@
+from jupyterhub_airlock.egress import (
+    EgressStore,
+    Egress,
+    EgressStatus,
+    is_valid_egress_id,
+)
+import json
+import pytest
+
+
+@pytest.mark.parametrize(
+    "id,valid",
+    [
+        ("egress-1", True),
+        ("123", True),
+        ("a1", True),
+        ("-123", False),
+        ("abc 123", False),
+    ],
+)
+@pytest.mark.parametrize("raise_invalid", [True, False])
+def test_is_valid_egress_id(id, valid, raise_invalid):
+    if not valid and raise_invalid:
+        with pytest.raises(ValueError) as exc_info:
+            is_valid_egress_id(id)
+        assert exc_info.value.args[0] == f"Invalid egress id {id}"
+    else:
+        assert is_valid_egress_id(id, raise_invalid) == valid
+
+
+def test_new_egress_invalid(tmp_path):
+    with pytest.raises(ValueError) as exc_info:
+        Egress("-123", tmp_path)
+    assert exc_info.value.args[0] == "Invalid egress id -123"
+
+
+def test_egressstore_new(tmp_path):
+    egress_id = "egress-1"
+    store = EgressStore(tmp_path)
+    assert store.list() == []
+
+    e = store.new_egress(egress_id)
+    assert store.list() == [egress_id]
+    assert e.id == egress_id
+    assert e.path == tmp_path / egress_id
+    assert sorted(str(p.relative_to(tmp_path)) for p in tmp_path.rglob("*")) == [
+        egress_id,
+        f"{egress_id}/files",
+        f"{egress_id}/metadata.json",
+    ]
+
+    with open(str(tmp_path / egress_id / "metadata.json")) as f:
+        assert json.load(f) == {"id": egress_id, "status": "new", "files": []}
+
+
+def test_egressstatus_transitions(tmp_path):
+    egress_id = "egress-1"
+    store = EgressStore(tmp_path)
+    e = store.new_egress(egress_id)
+
+    with pytest.raises(ValueError) as exc_info:
+        e.set_status(EgressStatus.ACCEPTED)
+    assert (
+        exc_info.value.args[0] == "Current status new, new status accepted is invalid"
+    )
+
+    with pytest.raises(ValueError) as exc_info:
+        e.set_status(EgressStatus.REJECTED)
+    assert (
+        exc_info.value.args[0] == "Current status new, new status rejected is invalid"
+    )
+
+
+def test_egressstore_add_files(tmp_path):
+    egress_id = "egress-1"
+    store = EgressStore(tmp_path)
+    e = store.new_egress(egress_id)
+    assert e.path == tmp_path / egress_id
+
+    (e.path / "files" / "hello.txt").write_text("hello")
+    (e.path / "files" / '{"}').mkdir()
+    (e.path / "files" / '{"}' / "world.txt").write_text("world")
+
+    e.add_files()
+
+    assert sorted(str(p.relative_to(tmp_path)) for p in tmp_path.rglob("*")) == [
+        egress_id,
+        f"{egress_id}/files",
+        f"{egress_id}/files/hello.txt",
+        f'{egress_id}/files/{{"}}',
+        f'{egress_id}/files/{{"}}/world.txt',
+        f"{egress_id}/metadata.json",
+    ]
+
+    with open(str(tmp_path / egress_id / "metadata.json")) as f:
+        assert json.load(f) == {
+            "id": egress_id,
+            "status": "pending",
+            "files": [
+                {
+                    "path": "hello.txt",
+                    "sha256sum": "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824",
+                },
+                {
+                    "path": '{"}/world.txt',
+                    "sha256sum": "486ea46224d1bb4fb680f34f7c9ad96a8f24ec88be73ea8e5a6c65260e9cb8a7",
+                },
+            ],
+        }
+
+        with pytest.raises(ValueError) as exc_info:
+            e.add_files()
+        assert (
+            exc_info.value.args[0]
+            == "Can only add files to status new, current status pending"
+        )
